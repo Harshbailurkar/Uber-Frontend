f7788daaf2ab829ee9c89f34dff14228095e2fbe
Harsh Bailurkar <harshbailurkar@gmail.com>
2025-01-02 13:20:09 +0530

    add search functionality

---
+++ src/components/LocationSearchPanel.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import { IoLocation } from "react-icons/io5";
+function LocationSearchPanel() {
+  return (
+    <div>
+      {/* this is sample data */}
+      <div className="flex items-center space-x-2 mb-4 justify-start">
+        <h2 className="bg-[#eee] p-3 rounded-full">
+          <IoLocation />
+        </h2>
+        <h4 className="font-medium">Vyankatesh Galli Ajara</h4>
+      </div>
+      <div className="flex items-center space-x-2 mb-4 justify-start">
+        <h2 className="bg-[#eee] p-3 rounded-full">
+          <IoLocation />
+        </h2>
+        <h4 className="font-medium">Vyankatesh Galli Ajara</h4>
+      </div>
+      <div className="flex items-center space-x-2 mb-4 justify-start">
+        <h2 className="bg-[#eee] p-3 rounded-full">
+          <IoLocation />
+        </h2>
+        <h4 className="font-medium">Vyankatesh Galli Ajara</h4>
+      </div>
+      <div className="flex items-center space-x-2 mb-4 justify-start">
+        <h2 className="bg-[#eee] p-3 rounded-full">
+          <IoLocation />
+        </h2>
+        <h4 className="font-medium">Vyankatesh Galli Ajara</h4>
+      </div>
+      <div className="flex items-center space-x-2 mb-4 justify-start">
+        <h2 className="bg-[#eee] p-3 rounded-full">
+          <IoLocation />
+        </h2>
+        <h4 className="font-medium">Vyankatesh Galli Ajara</h4>
+      </div>
+    </div>
+  );
+}
+
+export default LocationSearchPanel;
--- src/pages/UserHome.jsx
+++ src/pages/UserHome.jsx
@@ -8,6 +8,7 @@ import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
 import { RiArrowDownWideLine } from "react-icons/ri";
 import { SlArrowDown, SlArrowUp } from "react-icons/sl";
+import LocationSearchPanel from "../components/LocationSearchPanel";
 
 function UserHome() {
   const user = useSelector((state) => state.user);
@@ -22,10 +23,14 @@ function UserHome() {
     if (visible) {
       gsap.to(PannelRef.current, {
         height: "70%",
+        padding: "24px",
+        opacity: 1,
       });
     } else {
       gsap.to(PannelRef.current, {
         height: "0%",
+        padding: "0px",
+        opacity: 0,
       });
     }
   }, [visible]);
@@ -41,7 +46,12 @@ function UserHome() {
 
   return (
     <div className="h-screen w-screen flex flex-col relative">
-      <img src={UberLogo} alt="" className="w-16 absolute left-5 top-5" />
+      <div className="flex flex-row items-center justify-end">
+        <img src={UberLogo} alt="" className="w-16 absolute left-5 top-5" />
+        <div className="absolute  right-5 top-5 font-bold bg-[#004D40] text-white w-10 h-10 flex items-center justify-center rounded-full">
+          {user.fullName.firstName[0].toUpperCase()}
+        </div>
+      </div>
       <div className="h-screen w-screen">
         {/* image for temporary use */}
         <img
@@ -51,17 +61,20 @@ function UserHome() {
         />
       </div>
       <div className=" absolute flex flex-col h-screen justify-end top-0 w-full">
-        <div className="h-[30%]  bg-white p-5 relative">
-          <h5 className="p-1">
-            {visible ? (
-              <SlArrowDown size={17} onClick={() => setVisible(false)} />
-            ) : (
-              <SlArrowUp size={17} onClick={() => setVisible(true)} />
-            )}
-          </h5>
-          <h4 className="text-2xl font-semibold">Find Trip</h4>
+        <div className="h-[30%]  bg-white p-5 relative rounded-t-2xl">
+          <div className="flex justify-between">
+            <h4 className="text-2xl font-semibold">Find Trip</h4>
+            <h5 className="p-1">
+              {visible ? (
+                <SlArrowDown size={17} onClick={() => setVisible(false)} />
+              ) : (
+                <SlArrowUp size={17} onClick={() => setVisible(true)} />
+              )}
+            </h5>
+          </div>
+
           <form action={actionFunction}>
-            <div className="line absolute h-16 w-1 top-[45%] left-10 bg-gray-800 rounded-full "></div>
+            <div className="line absolute h-14 w-1 top-[45%] left-10 bg-gray-800 rounded-full "></div>
             <input
               type="text"
               name="pickupLocation"
@@ -78,7 +91,9 @@ function UserHome() {
             />
           </form>
         </div>
-        <div ref={PannelRef} className={`h-[70%] bg-red-500 `}></div>
+        <div ref={PannelRef} className={`h-[70%] bg-white `}>
+          <LocationSearchPanel />
+        </div>
       </div>
     </div>
   );
